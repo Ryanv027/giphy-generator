aad268d8e9b5fcc009e0fb4c8a9cc8e4898366b2
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-03-07 15:25:55 -0800

    working still-animate buttons

--- javascript.js
+++ javascript.js
@@ -24,11 +24,27 @@ $('#submit').on('click', function() {
         }).then(function(response) {
             console.log(response)
             for (var i = 0; i < 10; i++) {
-                var giphy = response.data[i].images.original.url
+                var giphy = response.data[i].images.original_still.url
+                var giphy1 = response.data[i].images.original.url
                 var rating = response.data[i].rating
                 console.log(giphy + rating)
-                $('#display').append(`<img src="${giphy}" class="gif" data-state="still" style="height: 300px; width:300px;"><p class="rating">Rating: ${rating}</p>`)
+                $('#display').append(`<img src="${giphy}" class="gif" data-still="${giphy}" data-animate="${giphy1}" data-state="still" style="height: 300px; width:300px;"><p class="rating">Rating: ${rating}</p>`)
             }
+            $('.gif').on('click', function() {
+
+                var state = ($(this).attr('data-state'))
+
+                console.log(state)
+
+                if (state === 'still') {
+                    console.log('working')
+                    $(this).attr('src', $(this).attr('data-animate'))
+                    $(this).attr('data-state', 'animate')
+                } else if (state === 'animate') {
+                    $(this).attr('src', $(this).attr('data-still'))
+                    $(this).attr('data-state', 'still')
+                }
+            })
         })
 
     })
